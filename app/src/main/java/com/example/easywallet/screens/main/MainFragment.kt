fb2ab7268d18efa9fc92923f76136436c597f8b3
marcosmartinezfco <marcosmartinezfco@gmail.com>
2022-02-19 20:20:59 +0100

    configuracion inicial

---
+++ app/src/main/java/com/example/easywallet/screens/main/MainFragment.kt
@@ -0,0 +1,24 @@
+package com.example.easywallet.screens.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.easywallet.R
+import com.example.easywallet.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        return binding.root
+    }
+}
